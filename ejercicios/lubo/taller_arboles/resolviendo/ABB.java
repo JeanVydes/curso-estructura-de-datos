f458e5bf9eb6f4f35ec1d72404d9e55c0c6bc80c
Jean Vides <videsrudas@proton.me>
2026-05-05 12:19:15 -0500

    add: taller resolviend

--- ejercicios/lubo/taller_arboles/resolviendo/ABB.java
+++ ejercicios/lubo/taller_arboles/resolviendo/ABB.java
@@ -73,26 +73,47 @@ public class ABB {
             return new Nodo(dato, prioridad);
         }
 
-        String primerCaracter = nodoActual.dato.charAt(0) + "";
-        if (dato.charAt(0) + "".compareTo(primerCaracter) < 0) {
+        // Comparamos toda la cadena de texto
+        // No solo el primer digito
+        // Se trata en conjunto, como una sola unidad
+        // Como una suma de todas sus partes
+        if (dato.compareTo(nodoActual.dato) < 0) {
             nodoActual.izquierdo = insertarRecursivo(nodoActual.izquierdo, dato, prioridad);
-        } else if (dato.charAt(0) + "".compareTo(primerCaracter) > 0) {
+        } else if (dato.compareTo(nodoActual.dato) > 0) {
             nodoActual.derecho = insertarRecursivo(nodoActual.derecho, dato, prioridad);
         }
 
         return nodoActual; // No se permiten valores duplicados
 
     }
-    
+
     public static void main(String[] args) {
         ABB arbol = new ABB();
         // 1. Construccion del árbol (dato, prioridad)
         arbol.insertar("A1", "N");
+        // ESTE NODO NO SE VA A INSERTAR, PORQUE YA EXISTE UN NODO CON EL DATO "A1"
         arbol.insertar("A1", "N");
+
+        // Algunos pensarian que este nodo NO SE va a insertar,
+        // Pero si lo hara, no es correcto pensar que solamente porque comienza con
+        // A ya existe, porque el dato completo es "A10" y no "A1"
+        // Esto se llama lexiografia
+        // comparamos A con A, 1 con 1, y luego 0 con nada, y como 0 es mayor que nada,
+        // entonces "A10" es mayor que "A1"
+        arbol.insertar("A10", "N");
+
+        // Caso del taller
+        arbol.insertar("C150", "N");
+        // Este se convertira en el hijo izquierdo de C150, porque "C15" es menor que
+        // "C150"
+        // La solucion para el caso del taller, era primero eliminar el nodo "C150" y
+        // luego insertar "C15", pero no es necesario eliminarlo, porque el ABB se
+        // encarga de colocar cada nodo en su lugar correcto
+        arbol.insertar("C15", "N");
 
         arbol.inOrdenPrioritarioRecursivo(arbol.raiz);
 
-        Nodo nodo = arbol.buscarRecursivo(arbol.raiz, "C10");
+        Nodo nodo = arbol.buscarRecursivo(arbol.raiz, "A10");
         if (nodo != null) {
             System.out.println("1");
         } else {
